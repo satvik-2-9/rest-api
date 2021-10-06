3be45abefb29a0a3e7df27107ca4a7a2010f7aa4
unknown <satvikbatra@gmail.com>
2021-10-06 16:01:45 +0530

    adding user login & jwt signing

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
-
+const jwt = require('jsonwebtoken')
 
 /* the idea of salting in password authentication is that 
    if the password is weak and maybe someone could figure out thr 
@@ -13,11 +13,11 @@ const User = require('../models/user');
 */
 
 router.post('/signup', (req, res, next) => {
-                    /* 10 salting rounds */
+    /* 10 salting rounds */
     User.find({ email: req.body.email })
         .exec()
         .then(user => {
-            if (user.length>=1) {
+            if (user.length >= 1) {
                 return res.status(409).json({
                     message: 'Mail exists'
                 });
@@ -48,10 +48,54 @@ router.post('/signup', (req, res, next) => {
                             });
                     }
                 });
-        }
-    })
+            }
+        })
     
-})
+});
+
+router.post('/login', (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                /* we got no user */
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+                if (result) {
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userID: user[0]._id
+                    },
+                        ""+process.env.JWT_KEY, /* helping the server read it as string ig  */
+                        {
+                            expiresIn: "1h"
+                        }
+                    );
+                    return res.status(200).json({
+                        message: 'Auth successful',
+                        token:token
+                    });
+                }
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
+});
 
 
 router.delete('/:userID', (req, res, next) => {
